8ae9a766318286f9cd099a3abfc4bba559fe8e4f
Nikita Kotsur <nkotsur@tula.co>
2024-07-29 20:25:46 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function toReadable (number) {
-  
+  const numberToWords = require('num-words');
+  let g = numberToWords(number)
+  let f = g.split(' ').filter(item => item !== 'and').join(' ');
+  return f
 }
